164241fb98c162d673a1da986c056cb2dc486cc1
Peter Queenan <peter_queenan@trimble.com>
2020-05-19 11:38:02 -0700

    Commint on local

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -8,6 +8,7 @@ namespace ConsoleApp1
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine("I'm back again!");
+            Console.WriteLine("On local");
         }
     }
 }
