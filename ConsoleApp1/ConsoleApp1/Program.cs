5b71a2f0ae94796817053e4514e4ab5fd962700f
Peter Queenan <peter_queenan@trimble.com>
2020-05-19 11:06:39 -0700

    To my local

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -7,7 +7,7 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            Console.WriteLine("I'm back!");
+            Console.WriteLine("I'm back again!");
         }
     }
 }
